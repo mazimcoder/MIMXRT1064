a2b045e590781b39cfaea5d395111b7dee9fa205
Mustafa ABDEL AZIM <info@mustafaz.com>
2021-06-10 18:32:51 +0200

    Raw C 1.1

---
+++ MIMXRT1064C_V1.1_Mustafa/source/MIMXRT1064C_V1.1_Mustafa.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MIMXRT1064.h"
+#include "fsl_debug_console.h"
+
+
+
+
+
+int main(void) {
+
+    /* Init board hardware. */
+    BOARD_ConfigMPU();
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+#endif
+
+    PRINTF("Hello World\n");
+
+
+
+
+
+
+    /* Force the counter to be placed into memory. */
+    volatile static int i = 0 ;
+    /* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+        i++ ;
+        /* 'Dummy' NOP to allow source level single stepping of
+            tight while() loop */
+        __asm volatile ("nop");
+    }
+    return 0 ;
+}
